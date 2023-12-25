d0e0c745b472ddbe3e4f5073c4d2271bbd08313a
Milo-Shen <Milo-Shen@outlook.com>
2023-12-25 18:17:16 +0800

    feat: optimize the analyze data

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -18,9 +18,10 @@ fn main() {
     let start_time = Instant::now();
     chart.initialize_tree_from_raw_data(mock_data);
     chart.generate_basic_layout();
+    let data = chart.get_node_linked_list();
     let duration = start_time.elapsed();
     println!("build org chart time {:?}", duration);
-    let data = chart.get_node_linked_list();
-    println!("{:#?}", data);
+    let print: Vec<_> = data.iter().map(|x| (x.borrow().id, x.borrow().x, x.borrow().y, x.borrow().width, x.borrow().height)).collect();
+    println!("{:#?}", print);
     println!("Hello, world!");
 }
