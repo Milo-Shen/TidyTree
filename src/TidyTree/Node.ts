917243f1e69cc8ac03c3a7b318b15e0ed3766a04
Milo-Shen <Milo-Shen@outlook.com>
2023-12-26 21:40:32 +0800

    feat: update the defination of Node

--- src/TidyTree/Node.ts
+++ src/TidyTree/Node.ts
@@ -9,6 +9,18 @@ export class Node {
   bounding_box_w: number;
   parent?: Node;
   children: Array<Node>;
+  // define the tidy info
+  thread_left?: Node;
+  thread_right?: Node;
+  extreme_left?: Node;
+  extreme_right?: Node;
+  shift_acceleration: number;
+  shift_change: number;
+  modifier_to_subtree: number;
+  modifier_thread_left: number;
+  modifier_thread_right: number;
+  modifier_extreme_left: number;
+  modifier_extreme_right: number;
 
   constructor(id: string, w: number = 0, h: number = 0) {
     this.id = id;
@@ -21,5 +33,12 @@ export class Node {
     this.bounding_box_w = 0;
     this.parent = undefined;
     this.children = [];
+    this.shift_acceleration = 0;
+    this.shift_change = 0;
+    this.modifier_to_subtree = 0;
+    this.modifier_thread_left = 0;
+    this.modifier_thread_right = 0;
+    this.modifier_extreme_left = 0;
+    this.modifier_extreme_right = 0;
   }
 }
