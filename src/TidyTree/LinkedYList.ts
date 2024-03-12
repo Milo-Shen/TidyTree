8c303d2efb675c00b9f935cfe7c41607e9dd0685
Milo-Shen <Milo-Shen@outlook.com>
2024-03-12 21:54:25 +0800

    feat: update LinkedYList

--- src/TidyTree/LinkedYList.ts
+++ src/TidyTree/LinkedYList.ts
@@ -3,10 +3,10 @@ export class LinkedYList {
   pos_y: number;
   next?: LinkedYList;
 
-  constructor(index: number, pos_y: number) {
+  constructor(index: number, pos_y: number, node?: LinkedYList) {
     this.index = index;
     this.pos_y = pos_y;
-    this.next = undefined;
+    this.next = node;
   }
 
   bottom() {
@@ -15,5 +15,19 @@ export class LinkedYList {
 
   pop() {
     return this.next;
+  }
+
+  update(index: number, pos_y: number) {
+    let node: LinkedYList = this;
+
+    while (node.pos_y <= pos_y) {
+      if (node.next) {
+        node = node.next;
+      } else {
+        return new LinkedYList(index, pos_y);
+      }
+    }
+
+    new LinkedYList(index, pos_y, node);
   }
 }
