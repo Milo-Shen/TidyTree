01dc8d05e8552ea42264ba94b296681d8b11d08b
Milo-Shen <Milo-Shen@outlook.com>
2024-04-25 18:00:46 +0800

    feat: add rust code

---
+++ rust/src/contour.rs
@@ -0,0 +1,25 @@
+//  use local types
+use crate::tidy_tree::Node;
+
+#[derive(Debug)]
+pub struct Contour {
+    is_left: bool,
+    current: Option<Node>,
+    modifier_sum: f32,
+}
+
+impl Contour {
+    pub fn new(is_left: bool, node: Option<Node>) -> Contour {
+        let mut modifier_sum = 0.0;
+
+        if node.is_some() {
+            let tidy = node.as_ref().unwrap();
+        }
+
+        Contour {
+            is_left,
+            current: node,
+            modifier_sum,
+        }
+    }
+}
