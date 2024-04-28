228a38e3ccd2186b0c3ca8e12ec05915fda798f9
Milo-Shen <Milo-Shen@outlook.com>
2024-04-28 13:19:58 +0800

    feat: refactor node.rs

--- rust/src/node.rs
+++ rust/src/node.rs
@@ -2,29 +2,6 @@ use std::cell::RefCell;
 use std::rc::{Rc, Weak};
 
 #[derive(Debug)]
-pub enum NodeType {
-    NORMAL,
-    LITE,
-    BATCH,
-    EXTEND,
-}
-
-#[derive(Debug)]
-pub struct Node {
-    pub id: i64,
-    pub width: f32,
-    pub height: f32,
-    pub x: f32,
-    pub y: f32,
-    pub relative_x: f32,
-    pub relative_y: f32,
-    pub bounding_box_w: f32,
-    pub parent: Weak<RefCell<Node>>,
-    pub children: Vec<Rc<RefCell<Node>>>,
-    pub mode: NodeType,
-}
-
-#[derive(Debug)]
 pub struct TidyInfo {
     pub thread_left: Option<Node>,
     pub thread_right: Option<Node>,
@@ -59,4 +36,27 @@ impl TidyInfo {
             prev_modified_sum: 0.0,
         }
     }
+}
+
+#[derive(Debug)]
+pub enum NodeType {
+    NORMAL,
+    LITE,
+    BATCH,
+    EXTEND,
+}
+
+#[derive(Debug)]
+pub struct Node {
+    pub id: i64,
+    pub width: f32,
+    pub height: f32,
+    pub x: f32,
+    pub y: f32,
+    pub relative_x: f32,
+    pub relative_y: f32,
+    pub bounding_box_w: f32,
+    pub parent: Weak<RefCell<Node>>,
+    pub children: Vec<Rc<RefCell<Node>>>,
+    pub mode: NodeType,
 }
