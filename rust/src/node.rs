42530a3ff88fb77a5858d7764b76ce0c2b23a4ac
Milo-Shen <Milo-Shen@outlook.com>
2024-04-28 13:18:18 +0800

    feat: add TidyInfo

--- rust/src/node.rs
+++ rust/src/node.rs
@@ -23,3 +23,40 @@ pub struct Node {
     pub children: Vec<Rc<RefCell<Node>>>,
     pub mode: NodeType,
 }
+
+#[derive(Debug)]
+pub struct TidyInfo {
+    pub thread_left: Option<Node>,
+    pub thread_right: Option<Node>,
+    pub extreme_left: Option<Node>,
+    pub extreme_right: Option<Node>,
+    pub shift_acceleration: f32,
+    pub shift_change: f32,
+    pub modifier_to_subtree: f32,
+    pub modifier_thread_left: f32,
+    pub modifier_thread_right: f32,
+    pub modifier_extreme_left: f32,
+    pub modifier_extreme_right: f32,
+    // to accelerate the speed of second walk
+    pub prev_modified_sum: f32,
+}
+
+impl TidyInfo {
+    pub fn new() -> TidyInfo {
+        TidyInfo {
+            thread_left: None,
+            thread_right: None,
+            extreme_left: None,
+            extreme_right: None,
+            shift_acceleration: 0.0,
+            shift_change: 0.0,
+            modifier_to_subtree: 0.0,
+            modifier_thread_left: 0.0,
+            modifier_thread_right: 0.0,
+            modifier_extreme_left: 0.0,
+            modifier_extreme_right: 0.0,
+            // to accelerate the speed of second walk
+            prev_modified_sum: 0.0,
+        }
+    }
+}
