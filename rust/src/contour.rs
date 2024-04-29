edc143292b519e847947bf7c3b8ca24504d54d35
Milo-Shen <Milo-Shen@outlook.com>
2024-04-29 17:21:31 +0800

    feat: update Contour

--- rust/src/contour.rs
+++ rust/src/contour.rs
@@ -8,16 +8,20 @@ use crate::node::Node;
 #[derive(Debug)]
 pub struct Contour {
     is_left: bool,
-    current: Option<Rc<RefCell<Node>>>,
+    current: Rc<RefCell<Node>>,
     modifier_sum: f32,
 }
 
 impl Contour {
-    pub fn new(is_left: bool, node: Option<Rc<RefCell<Node>>>) -> Contour {
+    pub fn new(is_left: bool, node: Rc<RefCell<Node>>) -> Contour {
         let mut modifier_sum = 0.0;
 
         if node.is_some() {
-            let tidy = node.as_ref().unwrap();
+            let tidy = node.borrow().tidy.as_ref();
+            
+            if tidy.is_some() {
+                modifier_sum = tidy.unwrap().modifier_to_subtree;
+            }
         }
 
         Contour {
