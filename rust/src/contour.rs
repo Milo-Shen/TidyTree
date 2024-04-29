2092ab993b731abac16436895b509647603c8374
Milo-Shen <Milo-Shen@outlook.com>
2024-04-29 17:14:45 +0800

    feat: update enhancement

--- rust/src/contour.rs
+++ rust/src/contour.rs
@@ -1,15 +1,19 @@
+// use rust std
+use std::cell::RefCell;
+use std::rc::Rc;
+
 //  use local types
 use crate::node::Node;
 
 #[derive(Debug)]
 pub struct Contour {
     is_left: bool,
-    current: Option<Node>,
+    current: Option<Rc<RefCell<Node>>>,
     modifier_sum: f32,
 }
 
 impl Contour {
-    pub fn new(is_left: bool, node: Option<Node>) -> Contour {
+    pub fn new(is_left: bool, node: Option<Rc<RefCell<Node>>>) -> Contour {
         let mut modifier_sum = 0.0;
 
         if node.is_some() {
