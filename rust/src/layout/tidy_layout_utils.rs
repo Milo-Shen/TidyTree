4735283b9fb968994c6931d8a5cbb0ad2a6659ad
Milo-Shen <Milo-Shen@outlook.com>
2024-05-05 20:46:24 +0800

    feat: finish init_node

--- rust/src/layout/tidy_layout_utils.rs
+++ rust/src/layout/tidy_layout_utils.rs
@@ -3,11 +3,16 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 // use local types
-use crate::node::Node;
+use crate::node::{Node, TidyInfo};
 use crate::utils::bfs_traverse_tree;
 
 pub fn init_node(root: Option<Rc<RefCell<Node>>>) {
     bfs_traverse_tree(root, |node| {
-        node.borrow().x = 0.0;
+        let mut node_mut = node.borrow_mut();
+        node_mut.x = 0.0;
+        node_mut.y = 0.0;
+        node_mut.relative_x = 0.0;
+        node_mut.relative_y = 0.0;
+        node_mut.tidy = Some(TidyInfo::new());
     });
 }
