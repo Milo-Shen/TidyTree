c631d4310a269bcc762d67443889049d910616d5
Milo-Shen <Milo-Shen@outlook.com>
2024-05-12 15:51:43 +0800

    feat: add LinkedYList

--- rust/src/layout.rs
+++ rust/src/layout.rs
@@ -1,3 +1,4 @@
 pub mod basic_layout;
 pub mod tidy_layout;
 pub mod tidy_layout_utils;
+pub mod linked_y_list;
---
+++ rust/src/layout/linked_y_list.rs
@@ -0,0 +1,45 @@
+pub struct LinkedYList {
+    pub index: usize,
+    pub y: f32,
+    pub next: Option<Box<LinkedYList>>,
+}
+
+impl LinkedYList {
+    pub fn new(index: usize, y: f32) -> Self {
+        LinkedYList {
+            index,
+            y,
+            next: None,
+        }
+    }
+
+    pub fn bottom(&self) -> f32 {
+        self.y
+    }
+
+    pub fn update(self, index: usize, y: f32) -> Self {
+        let mut list = self;
+
+        while list.y <= y {
+            if let Some(next) = list.next.take() {
+                list = *next;
+            } else {
+                return LinkedYList {
+                    index,
+                    y,
+                    next: None,
+                };
+            }
+        }
+
+        LinkedYList {
+            index,
+            y,
+            next: Some(Box::new(list)),
+        }
+    }
+
+    pub fn pop(mut self) -> Option<Self> {
+        self.next.take().map(|next| *next)
+    }
+}
