f98120c46edf23d63f67bc4605d1fd85bab1afde
Milo-Shen <Milo-Shen@outlook.com>
2024-08-25 15:45:11 +0800

    feat: refactor RandomCardViewPage.tsx

--- src/Page/RandomTSCardViewPage/RandomCardViewPage.tsx
+++ src/Page/RandomTSCardViewPage/RandomCardViewPage.tsx
@@ -18,24 +18,25 @@ import { execution_time } from "../../utils/execution_time";
 export default function RandomCardViewPage() {
   // state hook
   let [card_rust_list, set_rust_card_list] = useState(chartRenderDefaultData);
+
   const fetchCards = useCallback(async () => {
     // create mock data
-    let now = performance.now();
     let raw_data = execution_time("build mock data time: ", () => {
       return mock_org_chart_data(20, 3, true, [100, 200], [50, 100]);
     });
     console.log(raw_data);
 
     // build tidy data
-    let tidy_configuration = new TidyConfiguration();
-    let chart = new TidyTree(LayoutMode.Tidy, tidy_configuration);
-    now = performance.now();
-    chart.initialize_tree_from_raw_data(raw_data);
-    chart.generate_layout();
-    let card_list = chart.get_node_linked_list();
-    let line_list = chart.calculate_line_pos(chart.root);
-    let js_consume = performance.now() - now;
-    console.log(`JS process time: ${js_consume} ms`);
+    let { card_list, line_list } = execution_time("JS process time: ", () => {
+      let tidy_configuration = new TidyConfiguration();
+      let chart = new TidyTree(LayoutMode.Tidy, tidy_configuration);
+      chart.initialize_tree_from_raw_data(raw_data);
+      chart.generate_layout();
+      let card_list = chart.get_node_linked_list();
+      let line_list = chart.calculate_line_pos(chart.root);
+      return { card_list, line_list };
+    });
+
     console.log(card_list);
     console.log(line_list);
 
